a7cd0ea6de6e9abaa5fa8f8de3c520833d87610c
shawjia <shawjia@me.com>
2018-12-11 12:15:06 +0800

    🔧 update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,21 @@ module.exports = {
   extends: "airbnb-base",
   rules: {
     'no-console': ['off'],
+
+    'no-unused-vars': [
+      "error", { varsIgnorePattern: 'regeneratorRuntime' }
+    ],
   },
   globals: {
+    window: true,
+    document: true,
     App: true,
     Page: true,
     Component: true,
+    Behavior: true,
     getApp: true,
     getCurrentPages: true,
-    wx: true
+    wx: true,
+    worker: true
   }
 };
