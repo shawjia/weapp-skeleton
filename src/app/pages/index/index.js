a5656c0a6bada960dd9a5feca69dbab24b2ecd6a
shawjia <shawjia@me.com>
2018-12-11 12:10:23 +0800

    ✨ support async/await

--- src/app/pages/index/index.js
+++ src/app/pages/index/index.js
@@ -1,11 +1,27 @@
 // https://developers.weixin.qq.com/miniprogram/dev/framework/app-service/page.html
 
+import regeneratorRuntime from 'regenerator-runtime';
+
 const app = getApp();
 
 Page({
 
-  onShow() {
+  async onShow() {
     console.log('ohShow', app.globalData);
+
+    console.log('async...');
+
+    console.log(await this.later());
+
+    console.log('bye');
+  },
+
+  later() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('async finished: here I am');
+      }, 1000);
+    });
   },
 
   data: {
